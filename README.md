f25984e3e7e633c1b7d25ed1af6e1557bf63a69d
zhangna <zhangna@wilcom.com.cn>
2018-07-18 11:04:09 +0800

    a

--- README.md
+++ README.md
@@ -4,3 +4,7 @@
 ## 赚钱的目的是为了生存
 ## 生存的首要任务是吃饭
 ## 遂，此学习笔记取名food
+
+参考链接
+
+https://cnodejs.org/topic/55fac7eced1da72438e33a7d
--- test/__dirname.js
+++ test/__dirname.js
@@ -2,7 +2,9 @@
  * Created by ZhangNa on 2018/7/17.
  */
 console.log(__dirname);
-console.log(path.join(__dirname));
+
+var path = require('path');
+console.log(path.join(__dirname, 'aaa/bbb')); // 第一个参数和第二个参数的拼接
 
 
 
