9e44d0708d060779ceba5e3b79d6b8eb24377b01
zhangna <zhangna@wilcom.com.cn>
2018-07-18 10:50:11 +0800

    a

--- config/config.js
+++ config/config.js
@@ -1,28 +1,30 @@
-var global = {
+var config = {
+    "global": {
 
-    //访问端口
-    "port": 8085,
+        //访问端口
+        "port": 8085,
 
-    //登录
-    "security_hostname": "172.16.20.72",
-    "security_hostport": 8140,
+        //登录
+        "security_hostname": "172.16.20.72",
+        "security_hostport": 8140,
 
-    //报表
-    "report_hostname": "172.16.20.152",
-    "report_hostport": 8136,
+        //报表
+        "report_hostname": "172.16.20.152",
+        "report_hostport": 8136,
 
-    //wd
-    "wd_hostname": "172.16.20.64",
-    "wd_hostport": 8130,
+        //wd
+        "wd_hostname": "172.16.20.64",
+        "wd_hostport": 8130,
 
-    //conf
-    "conf_hostname": "172.16.20.64",
-    "conf_hostport": 8128,
+        //conf
+        "conf_hostname": "172.16.20.64",
+        "conf_hostport": 8128,
 
-    //asr
-    "asr_hostname": "172.16.20.179",
-    "asr_hostport": 8084
+        //asr
+        "asr_hostname": "172.16.20.179",
+        "asr_hostport": 8084
 
+    }
 }
 
-module.exports = global;
+module.exports = config;
--- test/requireConfigJs.js
+++ test/requireConfigJs.js
@@ -2,6 +2,7 @@
  * Created by ZhangNa on 2018/7/17.
  */
 
-var config = require('../config/config.js')
+var config = require('../config/config.js');
+//var config = require('../config/config'); // 这两行的效果一样，默认require获取js后缀的文件
 console.log(config);
 console.log(config.global.port);
