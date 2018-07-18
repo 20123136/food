61fc7a2f3bd89c266deaefdce3f4be91a9010b1e
zhangna <zhangna@wilcom.com.cn>
2018-07-18 10:44:58 +0800

    a

---
+++ config/config.js
@@ -0,0 +1,28 @@
+var global = {
+    
+    //访问端口
+    "port": 8085,
+
+    //登录
+    "security_hostname": "172.16.20.72",
+    "security_hostport": 8140,
+
+    //报表
+    "report_hostname": "172.16.20.152",
+    "report_hostport": 8136,
+
+    //wd
+    "wd_hostname": "172.16.20.64",
+    "wd_hostport": 8130,
+
+    //conf
+    "conf_hostname": "172.16.20.64",
+    "conf_hostport": 8128,
+
+    //asr
+    "asr_hostname": "172.16.20.179",
+    "asr_hostport": 8084
+
+}
+
+module.exports = global;
---
+++ test/requireConfigJs.js
@@ -0,0 +1,7 @@
+/**
+ * Created by ZhangNa on 2018/7/17.
+ */
+
+var config = require('../config/config.js')
+console.log(config);
+console.log(config.global.port);
