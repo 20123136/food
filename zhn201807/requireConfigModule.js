73ccbeb402cbb3ab50aab2df8f634ea36b5b4dbc
zhangna <zhangna@wilcom.com.cn>
2018-07-24 15:45:53 +0800

    a

--- zhn201807/requireConfigModule.js
+++ zhn201807/requireConfigModule.js
@@ -1,10 +1,12 @@
 /**
  * Created by ZhangNa on 2018/7/24.
  */
-
+    
+// https://www.npmjs.com/package/config
 var config = require('config');
 console.log(config);
 console.log(config.global.desc);
 console.log(config.get('global.desc'));
 console.log(config.has('global.desc'));
+
 
