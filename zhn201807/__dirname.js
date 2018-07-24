6cc1495d8427b5e1f09064330d093a1bf69898f0
zhangna <zhangna@wilcom.com.cn>
2018-07-24 10:22:38 +0800

    a

--- zhn201807/__dirname.js
+++ zhn201807/__dirname.js
@@ -1,20 +1,36 @@
 /**
  * Created by ZhangNa on 2018/7/17.
  */
-
+console.log('----------------------------');
 console.log(__dirname);//该行代码执行所在的目录
 console.log(__filename);//该行代码执行所在的文件路径
+console.log('----------------------------');
 
 var path = require('path');
-
 console.log(path.resolve(), path.resolve(''));//两者含义一样，都是输出当前node命令执行时所在的目录
-console.log(path.resolve('./zhn201807/__dirname.js'), path.resolve('zhn201807/__dirname.js'), path.resolve('config/config.js'));//当node命令在 D:\gitrepo\github\food> 目录下执行时
 
-console.log(path.resolve(__dirname, 'aaa/bbb'));
+console.log('----------------------------');
+console.log(path.resolve('./'), path.resolve('../'));//1.输出当前node命令执行的绝对路径；2.输出当前node命令执行的上级目录
 
-console.log(path.join(__dirname, 'aaa/bbb')); // 第一个参数和第二个参数的拼接
-console.log(path.join(), path.join(''), path.join('./'), path.join('../'));
-console.log(path.join('./zhn201807/__dirname.js'));
+console.log('----------------------------');
+console.log(path.resolve('./zhn201807/__dirname.js'), path.resolve('zhn201807/__dirname.js'));//当前node命令执行的路径和参数中的相对路径的拼接结果
+console.log(path.resolve('../zhn201807/__dirname.js'), path.resolve('../config/config.js'));//当前node命令执行的路径的上级目录和参数中的相对路径的拼接结果
+console.log('----------------------------');
+
+console.log(path.resolve(__dirname, 'aaa/bbb'));// 第一个参数(该行代码执行所在的目录)和第二个参数的拼接
+console.log(path.join(__dirname, 'aaa/bbb')); // 第一个参数(该行代码执行所在的目录)和第二个参数的拼接
+
+console.log('----------------------------');
+console.log(path.join(), path.join(''));// 都输出一个点：.
+
+console.log('----------------------------');
+console.log(path.join('./'), path.join('../')); //完整的纯字符串输出
+
+console.log('----------------------------');
+console.log(path.join('./zhn201807/__dirname.js'), path.join('zhn201807/__dirname.js')); // 输出结果一致，去掉./后的纯字符串输出
+
+console.log('----------------------------');
+console.log(path.join('../zhn201807/__dirname.js')); // 完整的纯字符串输出
 
 
 
